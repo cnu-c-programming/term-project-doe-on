d79c1819ddfc0e3e3284194cd12f0ee172afc352
doe-on <fovusshi01197@gmail.com>
2026-06-13 13:57:21 +0900

    Update student.h

--- student.h
+++ student.h
@@ -8,4 +8,16 @@ typedef struct Student {
     struct Student *next;
 } Student;
 
+Student *student_create(int id, const char *name, int score);
+
+void list_append(Student **head, Student *node);
+
+int list_count(const Student *head);
+
+Student *list_find(Student *head, int id);
+
+int list_delete(Student **head, int id);
+
+void list_free(Student **head);
+
 #endif
